1121fbdd4bde9bf738f0de24576ef2f945eed640
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2026-06-09 21:02:40 +0530

    Add element to array in binary search example

--- Search/binary_search.cpp
+++ Search/binary_search.cpp
@@ -26,7 +26,7 @@ int binarySearch(vector<int> &arr, int x) {
 }
 
 int main() {
-    vector<int> arr = { 2, 3, 4, 10, 40 };
+    vector<int> arr = { 2, 3, 4, 10, 40, 80 };
     int x = 10;
     int result = binarySearch(arr, x);
     if(result == -1) cout << "Element is not present in array";
