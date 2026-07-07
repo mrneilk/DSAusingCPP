2d09778ec95df5ab585b78989c3154699c6c80fb
Navraj Khanal <navrajkhanal2001@gmail.com>
2026-07-07 09:09:01 +0530

    test and update-add main

--- Search/BFS.cpp
+++ Search/BFS.cpp
@@ -23,3 +23,9 @@ void bfs(int startNode, const std::vector<std::vector<int>>& adj) {
         }
     }
 }
+
+int main() {
+    std::vector<std::vector<int>> adj = {{1,2},{0,3},{0,3},{1,2}};
+    bfs(0, adj);
+    return 0;
+}
