dfe141f62073a07477e0464879406ddb87de93d6
Navraj Khanal <66715404+mrneilk@users.noreply.github.com>
2026-07-23 17:49:11 +0530

    Implement nextGreaterElement function using stack

--- Stacks&Queue/monotonic_stack.cpp
+++ Stacks&Queue/monotonic_stack.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <vector>
+#include <stack>
+
+std::vector<int> nextGreaterElement(const std::vector<int>& nums) {
+    int n = nums.size();
+    std::vector<int> result(n, -1);
+    std::stack<int> st; // Stores indices
+
+    for (int i = 0; i < n; ++i) {
+        while (!st.empty() && nums[st.top()] < nums[i]) {
+            int prev_idx = st.top();
+            st.pop();
+            result[prev_idx] = nums[i];
+        }
+        st.push(i);
+    }
+    return result;
+}
